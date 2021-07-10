ddf4d298d2929460d5f1326e9839260a09b063ab
eunmi <eunmi208@konkuk.ac.kr>
2021-07-11 02:14:48 +0900

    feat : index.js express서버 구현

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const static = require('serve-static');
+const path = require('path');
+const router = express.Router();
+
+const app = express();
+app.listen(3000, function(){
+    console.log('server start');
+})
+
+app.use('/index.html',express.static(path.join(__dirname,'/index.html')));
+app.use('/css',express.static(path.join(__dirname,'/css')));
+app.use('/diary',express.static(path.join(__dirname,'/diary')));
+app.use('/user',express.static(path.join(__dirname,'/user')));
+app.use('/media',express.static(path.join(__dirname,'/media')));
+app.use('/summernote',express.static(path.join(__dirname,'/summernote')));
+
+
+app.use(router);
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, './index.html'));
+})
