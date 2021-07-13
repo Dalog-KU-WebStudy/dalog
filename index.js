ea2a920f9d535a5e78e2f8ac3aaf4c706848298c
eunmi <eunmi208@konkuk.ac.kr>
2021-07-13 11:24:02 +0900

    fix : 파일 구조 변경

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const static = require('serve-static');
 const path = require('path');
 const router = express.Router();
-const naver_login = require('./user/passport/naver');
+const naver_login = require('./passport/naver');
 var passport = require('passport')
 const KakaoStrategy = require('passport-kakao').Strategy;
 const naverStrategy = require('passport-naver').Strategy;
@@ -14,13 +14,15 @@ app.listen(3000, function(){
     console.log('server start');
 })
 
-app.use('/main',express.static(path.join(__dirname,'/index.html')));
-app.use('/css',express.static(path.join(__dirname,'/css')));
-app.use('/diary',express.static(path.join(__dirname,'/diary')));
-app.use('/user',express.static(path.join(__dirname,'/user')));
-app.use('/media',express.static(path.join(__dirname,'/media')));
-app.use('/summernote',express.static(path.join(__dirname,'/summernote')));
-app.use('/user/login', express.static(path.join(__dirname,'/user/login.html')))
+// app.use('/main',express.static(path.join(__dirname,'/index.html')));
+// app.use('/css',express.static(path.join(__dirname,'/css')));
+// app.use('/diary',express.static(path.join(__dirname,'/diary')));
+// app.use('/user',express.static(path.join(__dirname,'/user')));
+// app.use('/media',express.static(path.join(__dirname,'/media')));
+// app.use('/summernote',express.static(path.join(__dirname,'/summernote')));
+// app.use('/user/login', express.static(path.join(__dirname,'/user/login.html')))
+// app.use('/public', express.static(path.join(__dirname,'/public')));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs')
 app.use(session({
@@ -35,5 +37,5 @@ app.use(flash())
 app.use(router);
 naver_login(app);
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
