d0b4ac634ad49c0ac8459384178a865441b562e8
hwihwi99 <chamgrace@naver.com>
2021-07-20 10:06:47 +0900

    구글 로그안

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const static = require('serve-static');
 const path = require('path');
 const router = express.Router();
 const naver_login = require('./passport/naver');
-var passport = require('passport')
+const google_login = require('./passport/google');
+var passport = require('passport');
 const KakaoStrategy = require('passport-kakao').Strategy;
 const naverStrategy = require('passport-naver').Strategy;
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
 var session = require('express-session')
 var flash = require('connect-flash')
 
@@ -27,7 +29,12 @@ app.use(passport.session())
 app.use(flash())
 
 app.use(router);
+
+
+
 naver_login(app);
+google_login(app);
+
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
