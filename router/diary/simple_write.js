8b71ce4e48f5721d8c797bac2b54c4c0f0e9bb95
hwihwi99 <chamgrace@naver.com>
2021-08-11 13:55:42 +0900

    fix : 한줄일기 진행중

--- router/diary/simple_write.js
+++ router/diary/simple_write.js
@@ -3,11 +3,19 @@ const mysql = require('mysql');
 const connection = mysql.createConnection(dbconfig);
 connection.connect();
 const path = require('path');
+<<<<<<< refs/remotes/origin/develop
 
 module.exports=function(router){
     router.post('/question',(req,res)=> {
 
         console.log("simple write post");
+=======
+const router = require('./view');
+
+module.exports=function(router){
+    router.post('/',(req,res)=>{
+        console.log("simple_diary");
+>>>>>>> fix : 한줄일기 진행중
         var today = new Date();
         var dd = today.getDate();
         var mm = today.getMonth();
@@ -19,6 +27,7 @@ module.exports=function(router){
             mm = '0'+mm;
         }
         var date = yyyy+'-'+mm+'-'+dd;
+<<<<<<< refs/remotes/origin/develop
         const simple_title = req.body.selectQuestion;
         console.log("simple title : " + simple_title);
         console.log("=== req.body ===");
@@ -28,6 +37,14 @@ module.exports=function(router){
         if(req.user){
             var query = connection.query('insert into diary (user_id, diary_date, diary_title,diary_content) values(?,?,?,?)'
             ,[req.user.user_id,date,simple_title,content],(err,result)=>{
+=======
+        const title = req.body.title;
+        const content = req.body.content;
+    
+        if(req.user){
+            var query = connection.query('insert into diary (user_id, diary_date, diary_title,diary_content) values(?,?,?,?)'
+            ,[req.user.user_id,date,title,content],(err,result)=>{
+>>>>>>> fix : 한줄일기 진행중
                 if(err){
                     return res.status(500).json(err);
                 }else{
@@ -35,6 +52,7 @@ module.exports=function(router){
                 }
             })        
         }
+<<<<<<< refs/remotes/origin/develop
 
         else {
             res.send("<script>alert('로그인이 필요합니다.');location.href='/user/login';</script>");
@@ -42,3 +60,12 @@ module.exports=function(router){
 
     })
 }
+=======
+    
+        else {
+            res.send("<script>alert('로그인이 필요합니다.');location.href='/user/login';</script>");
+        }
+    
+    })    
+}
+>>>>>>> fix : 한줄일기 진행중
--- router/index.js
+++ router/index.js
@@ -43,6 +43,7 @@ module.exports = function (app, router, passport) {
         }
       );
     } else res.render("index.ejs", { profile: req.user, title: "여기를 눌러 타이틀을 수정하세요!" });
+
   });
 
   //메모부분
@@ -316,6 +317,11 @@ module.exports = function (app, router, passport) {
   const title_change = require("./user/title");
   title_change(router);
 
+<<<<<<< refs/remotes/origin/develop
   const simple_write = require('./diary/simple_write');
   simple_write(router);
+=======
+  const diary_simple_write = require("./diary/simple_write");
+  diary_simple_write(router);
+>>>>>>> fix : 한줄일기 진행중
 };
