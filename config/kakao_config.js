a69f11a224efc18931cae9cfeb592aa5b650ccff
YuJin <lyubliss@gmail.com>
2021-08-26 12:22:37 +0900

    fix: config 수정

--- config/kakao_config.js
+++ config/kakao_config.js
@@ -1,6 +1,6 @@
 module.exports={
     clientID : '169381b5824258f2de96b86eb52f827a',
     clientSecret : 'lBsCL5TbPK0yI8Epfyi4QMU2uCqWHgYW',
-    callback_url : 'http://localhost:3000/login/kakao/callback'
+    callback_url : 'http://ec2-13-124-209-233.ap-northeast-2.compute.amazonaws.com:3000/login/kakao/callback'
 
 }
--- config/naver_config.js
+++ config/naver_config.js
@@ -1,5 +1,5 @@
 module.exports={
     clientID : '3mIb0YZFHAtszmAtfvFg',
     clientSecret : 'Ye2LGnTQFf',
-    callback_url : 'http://localhost:3000/login/naver/callback'
+    callback_url : 'http://ec2-13-124-209-233.ap-northeast-2.compute.amazonaws.com:3000/login/naver/callback'
 }
