0fa518f6d5b163fb14ac5827a7e121e08bc320bd
eunmi <eunmi208@konkuk.ac.kr>
2021-08-26 16:27:22 +0900

    fix : 배포경로 수정

--- config/google_config.js
+++ config/google_config.js
@@ -1,6 +1,6 @@
 module.exports = {
     clientID : '32143467552-12l49nuqf2df6u6ubh234lb7qo1td7be.apps.googleusercontent.com',
     clientSecret : 'wShMkLgGkZfRLL6m2Df3WFp1',
-    callbackURL : 'http://ec2-15-164-72-209.ap-northeast-2.compute.amazonaws.com:3000/login/google/callback',
+    callbackURL : 'http://ec2-13-209-20-122.ap-northeast-2.compute.amazonaws.com:3000/login/google/callback',
 
 }
--- config/naver_config.js
+++ config/naver_config.js
@@ -1,5 +1,5 @@
 module.exports={
     clientID : '3mIb0YZFHAtszmAtfvFg',
     clientSecret : 'Ye2LGnTQFf',
-    callback_url : 'http://ec2-15-164-72-209.ap-northeast-2.compute.amazonaws.com:3000/login/naver/callback'
+    callback_url : 'http://ec2-13-209-20-122.ap-northeast-2.compute.amazonaws.com:3000/login/naver/callback'
 }
