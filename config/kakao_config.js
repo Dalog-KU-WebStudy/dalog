89b4e6bf3ff81c5e63ea8abca5288c5186cbf3c3
whiteee8 <85060905+whiteee8@users.noreply.github.com>
2021-09-09 02:08:49 +0900

    Delete config directory

--- config/kakao_config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    clientID : '169381b5824258f2de96b86eb52f827a',
-    clientSecret : 'lBsCL5TbPK0yI8Epfyi4QMU2uCqWHgYW',
-    callback_url : 'http://localhost:3000/login/kakao/callback'
-
-}
