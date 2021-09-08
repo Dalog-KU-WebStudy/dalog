5c1eb9a2623af8bd23f7bbf37981daa77f5da819
whiteee8 <85060905+whiteee8@users.noreply.github.com>
2021-09-09 02:08:31 +0900

    Delete config directory

--- config/naver_config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    clientID : '3mIb0YZFHAtszmAtfvFg',
-    clientSecret : 'Ye2LGnTQFf',
-    callback_url : 'http://ec2-13-209-20-122.ap-northeast-2.compute.amazonaws.com:3000/login/naver/callback'
-}
